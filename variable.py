5120d2b97cef486bca77f87d8055ad8384f1e1b4
thinthinhtet22 <thelthel1998@gmail.com>
2020-02-23 12:22:51 +0630

    variable & list

---
+++ variable.py
@@ -0,0 +1,57 @@
+
+
+>>> 2 + 2
+4 
+
+* / % + - (operator)
+
+adddition = 75 + 25
+substraction = 204 - 204
+multiplication = 100 * 0.5
+division = 90 / 9
+ power of 2 = 3 ** 2
+ the remainder of the division = 10 % 3
+ int - integer - 1, 2, 3, ---> 308930 etc 
+ float - 	- 1.0, 5.9, 6.0, 63.23, etc 
+ string - 	- 'Hello', "World", "A", "B",
+
+ a =1 
+ a (variable) = (assign) 1 (value)
+
+ width = 20 
+ height = 5 * 9
+ vol = width * height 
+ vol
+
+ sale = 1500
+ tax = 5 / 100
+ total_tax = slae * tax
+ total_tax
+ total_price = sale + total_tax
+ total_price
+
+ print('spam eggs')
+ print('don\'t')
+ print("doesn't")
+ print('"Yes"')
+ print("\"Yes,\" they said.")
+ print('"Isn\'t," they said')
+
+ print('"Isn\'t, they said"')
+
+
+
+
+s = 'First Line.\nSecondLIne'
+print(s)
+
+print("""\
+Usage : thingy
+		-a
+		-b
+		-c
+""")
+
+"""...""" or '''...'''
+
+
